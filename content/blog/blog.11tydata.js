0b1473e210a0e2365ef1ca19bafe669a020df345
二叉树树 <acofork@qq.com>
2026-07-04 05:04:50 +0800

    feat: PagesCMS 文章支持自定义 Slug / URL

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -3,4 +3,14 @@ module.exports = {
 		"posts"
 	],
 	"layout": "layouts/post.njk",
+	eleventyComputed: {
+		permalink: data => {
+			if (data.slug) {
+				const slug = data.slug.replace(/^\/+|\/+$/g, '');
+				return `/blog/${slug}/`;
+			}
+			// 默认使用文件路径作为 URL
+			return data.page.filePathStem.replace(/^\/content/, '') + '/';
+		}
+	}
 };
