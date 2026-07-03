5fa4728ad39a3a62e1828046be5a19e3f8bfb610
二叉树树 <acofork@qq.com>
2026-07-04 05:07:14 +0800

    feat: Slug 由文件名决定，PagesCMS 可直接编辑文件名

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -5,12 +5,8 @@ module.exports = {
 	"layout": "layouts/post.njk",
 	eleventyComputed: {
 		permalink: data => {
-			if (data.slug) {
-				const slug = data.slug.replace(/^\/+|\/+$/g, '');
-				return `/blog/${slug}/`;
-			}
-			// 默认使用文件路径作为 URL
-			return data.page.filePathStem.replace(/^\/content/, '') + '/';
+			// 用文件名作为 URL slug
+			return `/blog/${data.page.fileSlug}/`;
 		}
 	}
 };
